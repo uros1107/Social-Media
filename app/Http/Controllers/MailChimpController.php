527ec68fe4a6891be271008d6281e45b0adaa395
amarchan9655 <amar.chan9655@gmail.com>
2021-05-11 23:55:38 +0900

    Added

--- app/Http/Controllers/MailChimpController.php
+++ app/Http/Controllers/MailChimpController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use vendor\mailchimp\mailchimp\src\Mailchimp;
+use Vendor\Mailchimp\Mailchimp\Src\Mailchimp;
 
 class MailChimpController extends Controller
 {
