774d382c95bd743af29d9c3f2f59d9df9111fb85
amarchan9655 <amar.chan9655@gmail.com>
2021-05-13 09:26:14 +0900

    Added idol register

--- resources/views/idol/register.blade.php
+++ resources/views/idol/register.blade.php
@@ -5,24 +5,100 @@
 @section('styles')
 <style>
 .block-1 {
-    -moz-background-size: cover;
-    background-size: cover;
-    -o-background-size: cover;
-    height: 600px;
+    height: 850px;
 }
-.arrow {
-    margin-top: 10px;
-    margin-bottom: 15px;
+.block-2 {
+    background: #121212;
+    padding-top: 190px;
+    padding-bottom: 110px;
 }
-.mt-80 {
-    margin-top: 80px;
+.block-3 {
+    background: #171717;
+    padding-bottom: 60px;
 }
-.under-line {
-    height: 2px;
-    background: #D9BC84;
-    width: 120px;
-    margin-bottom: 15px;
-    margin-top: 12px;
+.bg-img {
+    height: 100vh;
+    object-fit: cover;
+    position: absolute;
+}
+.hand-img {
+    position: absolute;
+    top: 35px;
+    right: 0px;
+}
+.welcome-millionk {
+    margin-top: 300px;
+    margin-left: 70px;
+}
+.block-title {
+    font-size: 36px;
+    letter-spacing: 6px;
+    margin-bottom: 50px;
+}
+.horizontal-red-bar {
+    width: 100px;
+    height: 3px;
+    background-color: #FF335C;
+    margin: auto;
+    margin-bottom: 40px;
+}
+.text-color {
+    color: #fcfcfc!important;
+}
+.item-title {
+    margin: 30px 0px 20px;
+}
+.footer-text {
+    padding: 10px;
+    margin-left: 15px;
+}
+.footer-social {
+    padding: 10px;
+}
+.footer-subscribe {
+    width: 600px;
+}
+.footer-subscribe-part {
+    background: #121212;
+    padding: 10px 30px 0px;
+}
+
+@media screen and (max-width:425px) {
+    .footer-subscribe {
+        width: 100%!important;
+    }
+    .footer-subscribe-part {
+        padding: 10px 0px 0px!important;
+    }
+    .footer-subscribe-input {
+        height: 36px!important;
+        font-size: 12px!important;
+    }
+    .footer-subscribe-btn {
+        height: 36px!important;
+        font-size: 12px!important;
+    }
+    .footer-subscribe-icon {
+        top: 9px!important;
+    }
+    .footer-text {
+        margin-left: 0px!important;
+    }
+    .block-title {
+        font-size: 18px;
+    }
+    .item-title {
+        font-size: 16px;
+    }
+    p, h5 {
+        font-size: 14px;
+    }
+    .create-showcase {
+        margin-bottom: 35px;
+    }
+    a {
+        font-size: 14px;
+    }
 }
 </style>
 @endsection
@@ -30,33 +106,250 @@
 @section('content')
 <div class="content">
     <div>
-        <img src="{{ asset('assets/images/bg.png') }}" class="w-100">
+        <img class="bg-img w-100" src="{{ asset('assets/images/bg.png') }}">
     </div>
-    <div class="container">
+    <div class="container-fluid p-0" style="height: 100%!important">
         <div class="block-1">  
-        </div>  
-        <div class="block-2">
-        </div>
-        <div class="block-3 mt-80">
-        </div>
-        <div class="block-4">
-        </div>
-        <div class="block-5">
-        </div>
-    </div>
-    <div class="footer" style="margin-top:50px">
-        <footer>
-            <div class="container">
-                <div class="row">
-                    <div class="col-6">
-                        <p style="color:#B9BDDC;font-style:italic;font-size:18px;margin-top:5px">Copyright © 2021 LostCenturia.gg. Tous droits réservés. </p>
+            <div class="top desktop">
+                <img src="{{ asset('assets/images/top-left-img.png') }}">
+            </div>
+            <div class="row desktop m-0">
+                <div class="col-12 d-flex pr-0">
+                    <div class="welcome-millionk">
+                        <div class="">
+                            <span class="comming text-white">Welcome to Millionk</span>
+                        </div>
+                        <h1 class="text-white main-text">MEET THE WORLD'S FIRST<br> HALLYU CELEBRITY PLATFORM</h1>
+                        <h3 class="description">Create & Earn by Fulfilling personalized<br> videos from your fans worldwide.</h3>
+                        <div class="input-group mb-3 mt-5">
+                            <button class="btn btn-primary custom-btn" style="height: 45px" type="submit">APPLY NOW</button>
+                        </div>
                     </div>
-                    <div class="col-6 text-right">
-                        <p style="color:#B9BDDC;font-style:italic;font-size:12px">SW: Lost Centuria est une marque enregistrée par Com2Us. <br>Site fan non officiel et non affilié à Com2Us. </p>
+                    <div class="middle-hand">
+                        <img src="{{ asset('assets/images/hand.png') }}" class="hand-img">
                     </div>
                 </div>
             </div>
-        </footer>
+        </div>  
+        <div class="block-2 text-center">
+            <h1 class="text-color block-title">CONNECT WITH YOUR FRIENDS WORLDWIDE</h1>
+            <div class="horizontal-red-bar"></div>
+            <div class="container">
+                <div class="row m-0">
+                    <div class="col-12 col-sm-4 col-md-4 create-showcase">
+                        <img src="{{ asset('assets/images/create_showcase.png') }}" style="border-radius: 50%">
+                        <h4 class="text-main-color text-uppercase item-title">Create Showcase</h4>
+                        <p class="text-color">Your own pricing for personalized videos<br>for your fans</p>
+                    </div>
+                    <div class="col-12 col-sm-4 col-md-4 create-showcase">
+                        <img src="{{ asset('assets/images/showcase.png') }}" style="border-radius: 50%">
+                        <h4 class="text-main-color text-uppercase item-title">Showcase</h4>
+                        <p class="text-color">Choosen personalized videos you have<br>given for your fans</p>
+                    </div>
+                    <div class="col-12 col-sm-4 col-md-4 create-showcase">
+                        <img src="{{ asset('assets/images/next_gen.png') }}" style="border-radius: 50%">
+                        <h4 class="text-main-color text-uppercase item-title">Next-Gen</h4>
+                        <p class="text-color">Take the next step in connecting with<br>your fans through personalized videos</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="block-3">
+            <div class="text-center" style="margin-bottom: 60px">
+                <h1 class="text-color block-title" style="padding-top: 100px">APPLY TO JOIN MILLIONK AS AN IDOL NOW</h1>
+                <div class="horizontal-red-bar"></div>
+                <h5 class="text-color">If you are part of the Korean Wave sweeping the globe, you can apply<br>here and we will get in touch with you within 72hours.</h5>
+            </div>
+            <div class="container">
+                <form class="custom-form">
+                    <div class="row m-0">
+                        <div class="col-12 col-sm-6 col-md-6">
+                            <div class="inputWithIcon">
+                                <label class="input-label">Your Name</label>
+                                <input type="text" placeholder="Your name">
+                                <!-- <i class="fa fa-user fa-lg fa-fw" aria-hidden="true"></i> -->
+                                <img class="input-icon" src="{{ asset('assets/images/icons/user.png') }}">
+                            </div>
+                        </div>
+                        <div class="col-12 col-sm-6 col-md-6">
+                            <div class="inputWithIcon">
+                                <label class="input-label">Phone Number(Never Shared)</label>
+                                <input type="text" placeholder="Phone number">
+                                <img class="input-icon" src="{{ asset('assets/images/icons/phone.png') }}">
+                            </div>
+                        </div>
+                        <div class="col-12 col-sm-12 col-md-12">
+                            <div class="inputWithIcon">
+                                <label class="input-label">Email</label>
+                                <input type="text" placeholder="Email">
+                                <img class="input-icon" src="{{ asset('assets/images/icons/mail.png') }}">
+                            </div>
+                        </div>
+                        <div class="col-12 col-sm-12 col-md-12">
+                            <div class="inputWithIcon">
+                                <label class="input-label">Where can we find you?</label>
+                                <input type="text" placeholder="Twitter">
+                                <img class="input-icon" src="{{ asset('assets/images/icons/twitter.png') }}">
+                            </div>
+                        </div>
+                        <div class="col-12 col-sm-6 col-md-6">
+                            <div class="inputWithIcon">
+                                <label class="input-label">Your handle/Username</label>
+                                <input type="text" placeholder="User name">
+                                <img style="top:39px" class="input-icon" src="{{ asset('assets/images/icons/a.png') }}">
+                            </div>
+                        </div>
+                        <div class="col-12 col-sm-6 col-md-6">
+                            <div class="inputWithIcon">
+                                <label class="input-label">How many followers do you have?</label>
+                                <input type="text" placeholder="20">
+                                <img class="input-icon" src="{{ asset('assets/images/icons/user3.png') }}">
+                            </div>
+                        </div>
+                        <div class="col-12 col-sm-12 col-md-12">
+                            <div class="inputWithIcon">
+                                <label class="input-label">Anything else we should know about you?</label>
+                                <textarea class="custom-textarea" style="height:120px!important" placeholder="Let us know about you..."></textarea>
+                            </div>
+                        </div>
+                        <div class="col-12 mt-3">
+                            <button class="btn btn-primary custom-btn w-100" type="submit">Submit</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+            
+        </div>
+        <!-- mobile -->
+        <div class="m-top mobile">
+            <div class="top-bar">
+                <img class="logo-img" src="{{ asset('assets/images/top-left-img.png') }}">
+                <div class="right-side-icons">
+                    <i class="fa fa-search" style="color: #FF335C"></i>
+                    <i class="fa fa-bell-o text-white"></i>
+                    <i class="fa fa-navicon text-white"></i>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="footer">
+        <div class="container-fluid m-0" style="height: 100%!important">
+            <div class="row footer-subscribe-part">
+                <div class="col-12 col-sm-4 col-md-4 my-auto">
+                    <h5 class="text-color">Join our mailing list</h5>
+                    <p style="color:#898989">Be the first to know about the newest<br>stars & talents coming on MillionK</p>
+                </div>
+                <div class="col-12 col-sm-8 col-md-8" style="text-align: -webkit-right;">
+                    <form class="custom-form d-flex my-auto footer-subscribe" style="width: 600px">
+                        <div class="inputWithIcon" style="width: 73%">
+                            <input type="text" placeholder="Email" class="footer-subscribe-input">
+                            <img style="top: 16px" class="input-icon footer-subscribe-icon" src="{{ asset('assets/images/icons/a.png') }}">
+                        </div>
+                        <button class="btn btn-primary custom-btn m-auto footer-subscribe-btn" style="margin-right: 0px!important;width: 25%" type="submit">Subscribe</button>
+                    </form>
+                </div>
+            </div>
+            <div class="row desktop" style="padding: 10px 30px;background:#171717">
+                <div class="col-sm-8 col-md-8 d-flex" style="margin: 10px 0px">
+                    <img src="{{ asset('assets/images/top-left-img.png') }}" style="height: 46px;margin: 0px 10px;">
+                    <div class="footer-text">
+                        <p class="text-main-color mb-0">About</p>
+                    </div>
+                    <div class="footer-text">
+                        <p class="text-main-color mb-0">Request an Idol</p>
+                    </div>
+                    <div class="footer-text">
+                        <p class="text-main-color mb-0">FAQ</p>
+                    </div>
+                    <div class="footer-text">
+                        <p class="text-main-color mb-0">Help</p>
+                    </div>
+                    <div class="footer-text">
+                        <p class="text-main-color mb-0">Contact Us</p>
+                    </div>
+                </div>
+                <div class="col-sm-4 col-md-4 d-flex" style="justify-content: flex-end;margin: 10px 0px">
+                    <div class="footer-social">
+                        <img src="{{ asset('assets/images/icons/facebook.png') }}" style="height: 18px">
+                    </div>
+                    <div class="footer-social">
+                        <img src="{{ asset('assets/images/icons/instagram.png') }}" style="height: 18px">
+                    </div>
+                    <div class="footer-social">
+                        <img src="{{ asset('assets/images/icons/gmail.png') }}" style="height: 18px">
+                    </div>
+                </div>
+                <div class="col-12">
+                    <div style="height: 1px;background: #898989;width:100%"></div>
+                </div>
+                <div class="col-12 d-flex" style="margin: 20px 0px 10px;">
+                    <div class="my-auto">
+                        <p class="mb-0" style="color:#898989">Copyright © 2021 Lumiworks.pte.Ltd.All rights reserved.</p>
+                    </div>
+                    <div class="my-auto" style="position: absolute;right:15px">
+                        <a href="#" class="text-main-color mr-5">Privacy policy</a>
+                        <a href="#" class="text-main-color">Tearms of service</a>
+                    </div>
+                </div>
+            </div>
+            <div class="mobile">
+                <div class="row" style="background:#171717">
+                    <div class="col-6 d-flex" style="margin: 10px 0px">
+                        <img src="{{ asset('assets/images/top-left-img.png') }}" style="height: 46px;margin: 0px 10px;">
+                    </div>
+                    <div class="col-6 d-flex" style="margin: 10px 0px;justify-content: flex-end;">
+                        <div class="footer-social">
+                            <img src="{{ asset('assets/images/icons/facebook.png') }}" style="height: 18px">
+                        </div>
+                        <div class="footer-social">
+                            <img src="{{ asset('assets/images/icons/instagram.png') }}" style="height: 18px">
+                        </div>
+                        <div class="footer-social">
+                            <img src="{{ asset('assets/images/icons/gmail.png') }}" style="height: 18px">
+                        </div>
+                    </div>
+                    <div class="col-6">
+                        <div class="footer-text">
+                            <a class="text-main-color mb-0">About</a>
+                        </div>
+                    </div>
+                    <div class="col-6">
+                        <div class="footer-text">
+                            <a class="text-main-color mb-0">Help</a>
+                        </div>
+                    </div>
+                    <div class="col-6">
+                        <div class="footer-text">
+                            <a class="text-main-color mb-0">Request an Idol</a>
+                        </div>
+                    </div>
+                    <div class="col-6">
+                        <div class="footer-text">
+                            <a class="text-main-color mb-0">Contact Us</a>
+                        </div>
+                    </div>
+                    <div class="col-6">
+                        <div class="footer-text">
+                            <a class="text-main-color mb-0">FAQ</a>
+                        </div>
+                    </div>
+                    <div class="col-6">
+                        <div class="footer-text">
+                            <!-- <p class="text-main-color mb-0">About</p> -->
+                        </div>
+                    </div>
+                    <div class="col-12 text-center" style="padding: 10px 15px;border-top: 1px solid #2b2b2b;">
+                        <a href="#" class="text-main-color">Privacy policy</a>
+                    </div>
+                    <div class="col-12 text-center" style="padding: 10px 15px">
+                        <a href="#" class="text-main-color">Tearms of service</a>
+                    </div>
+                    <div class="col-12 text-center" style="padding: 10px 15px;margin-bottom:20px">
+                        <p class="mb-0" style="color:#898989">Copyright © 2021 Lumiworks.pte.Ltd<br>All rights reserved.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </div>
 
