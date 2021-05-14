13651b133a1098f38be3790c626bee611deed39a
amarchan9655 <amar.chan9655@gmail.com>
2021-05-14 13:32:20 +0900

    Added

--- resources/views/idol/register.blade.php
+++ resources/views/idol/register.blade.php
@@ -81,13 +81,16 @@
 
 @media screen and (max-width:768px) {
     .footer-subscribe {
-        width: 460px;
+        /* width: 460px!important; */
     }
     .welcome-millionk {
-        margin-left: 0px;
+        margin-left: 0px!important;
     }
-    .main-text {
-        font-size: 46px;
+    .m-main-text {
+        /* font-size: 46px!important; */
+        letter-spacing: 4px;
+        margin-bottom: 26px;
+        margin-top: 24px;
     }
 }
 
@@ -144,20 +147,19 @@
         margin-top: 200px;
         margin-left: 0px;
     }
-    .comming {
+    .m-comming {
         font-size: 10px;
     }
-    .comming:before {
+    .m-comming:before {
         content: "this this"
     }
-    .main-text {
+    .m-main-text {
         font-size: 20px!important;
     }
-    .description {
+    .m-description {
         font-size: 12px;
     }
 }
-
 </style>
 @endsection
 
@@ -192,10 +194,10 @@
                 <div class="col-12 d-flex">
                     <div class="welcome-millionk text-center">
                         <div class="">
-                            <span class="comming text-white">Welcome to Millionk</span>
+                            <span class="comming text-white m-comming">Welcome to Millionk</span>
                         </div>
-                        <h1 class="text-white main-text">MEET THE WORLD'S FIRST HALLYU CELEBRITY PLATFORM</h1>
-                        <h3 class="description">Create & Earn by Fulfilling personalized videos from your fans worldwide.</h3>
+                        <h1 class="text-white main-text m-main-text">MEET THE WORLD'S FIRST HALLYU CELEBRITY PLATFORM</h1>
+                        <h3 class="description m-description">Create & Earn by Fulfilling personalized videos from your fans worldwide.</h3>
                         <div class="input-group mb-3 mt-5" style="justify-content: center">
                             <button class="btn btn-primary custom-btn" style="height: 30px;font-size: 10px" type="submit">APPLY NOW</button>
                         </div>
