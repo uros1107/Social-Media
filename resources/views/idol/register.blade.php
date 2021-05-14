02fc1313dcf6f13af048d1b2ca3e03abe7f6afd6
amarchan9655 <amar.chan9655@gmail.com>
2021-05-14 13:25:07 +0900

    Added

--- resources/views/idol/register.blade.php
+++ resources/views/idol/register.blade.php
@@ -91,6 +91,7 @@
         letter-spacing: 4px;
         margin-bottom: 26px;
         margin-top: 24px;
+        font-weight: 500;
     }
 }
 
@@ -197,7 +198,7 @@
                         <div class="">
                             <span class="comming text-white m-comming">Welcome to Millionk</span>
                         </div>
-                        <h1 class="text-white m-main-text">MEET THE WORLD'S FIRST HALLYU CELEBRITY PLATFORM</h1>
+                        <p class="text-white m-main-text">MEET THE WORLD'S FIRST HALLYU CELEBRITY PLATFORM</p>
                         <h3 class="description m-description">Create & Earn by Fulfilling personalized videos from your fans worldwide.</h3>
                         <div class="input-group mb-3 mt-5" style="justify-content: center">
                             <button class="btn btn-primary custom-btn" style="height: 30px;font-size: 10px" type="submit">APPLY NOW</button>
