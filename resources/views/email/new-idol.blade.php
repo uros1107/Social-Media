c872a304d22a72da582e2094448aa81c5164bf18
amarchan <amar.chan9655@gmail.com>
2021-07-21 20:58:30 +0900

    Updated feedback

--- resources/views/email/new-idol.blade.php
+++ resources/views/email/new-idol.blade.php
@@ -1,42 +1,42 @@
 <h1>New Idol Registration Required!</h1>
    
-<h4>User Information:<h4>
+<h3>User Information:<h4>
 <div style="display:flex">
-    <h5>Full Name</h5>
+    <h4>Full Name : </h4>
     <p>{{ $idol_information['name'] }}</p>
 </div>
 <div style="display:flex">
-    <h5>Korean Name</h5>
+    <h4>Korean Name : </h4>
     <p>{{ $idol_information['k_name'] }}</p>
 </div>
 
 <div style="display:flex">
-    <h5>Email</h5>
+    <h4>Email : </h4>
     <p>{{ $idol_information['email'] }}</p>
 </div>
 
 <div style="display:flex">
-    <h5>Phone Number</h5>
+    <h4>Phone Number : </h4>
     <p>{{ $idol_information['phone'] }}</p>
 </div>
 
-<div style="display:flex">
-    <h5>Where can we find you?</h5>
+<div>
+    <h4>Where can we find you?</h4>
     <p>{{ $idol_information['where_find'] }}</p>
 </div>
 
-<div style="display:flex">
-    <h5>What's your username on that platform?</h5>
+<div>
+    <h4>What's your username on that platform?</h4>
     <p>{{ $idol_information['social_name'] }}</p>
 </div>
 
-<div style="display:flex">
-    <h5>How many followers do you have?</h5>
+<div>
+    <h4>How many followers do you have?</h4>
     <p>{{ $idol_information['followers'] }}</p>
 </div>
 
-<div style="display:flex">
-    <h5>Anything else we should know about you?</h5>
+<div>
+    <h4>Anything else we should know about you?</h4>
     <p>{{ $idol_information['info'] }}</p>
 </div>
 
